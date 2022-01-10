e823595c2883eaf7a547b852d89b146a81b98547
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-01-10 13:50:01 -0300

    C1 - criado componente Button

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    const { handleClick, value, numValue, name, id } = this.props;
+    return(
+      <button
+        type="button"
+        name={ name }
+        id={ id }
+        onClick={ handleClick }
+        value={ value }
+      >
+        {numValue}
+      </button>
+    )
+  }
+}
+
+export default Button;
