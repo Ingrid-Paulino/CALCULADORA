2918ec4ab4c07cb8067c5e15e5c98ae1beb2f392
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-01-21 06:45:40 -0300

    C3 - criado os componentes necessarios - tinha esquecido de salvar

--- src/App.js
+++ src/App.js
@@ -9,12 +9,24 @@ function App() {
   const [acumulador, setAcumulador] = useState(0)
   const [operador, setOperador] = useState(false)
 
+  // COMPONENTES
   //cria a tela da calculadora
   const tela = (valor, res) => {
     return(
-      
+      <div>
+        <span>{valor}</span> {/*Operação*/}
+        <span>{res}</span> {/*Resposta*/}
+      </div>
     )
   }
+
+  //cria os botoes
+  const Btn = (label, onClick)=> {
+    return(
+      <button onClick={onClick}>{ label }</button>
+    )
+  }
+
 
 
 
